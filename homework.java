dbec8b264dcc79ee2642e621e7246b36f962a40e
sohaib20030720 <sohaib.mousselmal@ensia.edu.dz>
2023-03-08 19:00:28 +0100

    adding button

---
+++ homework.java
@@ -0,0 +1,18 @@
+﻿import javax.swing.*;
+
+
+public class homework {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Number GUI"); // create a JFrame object with title "Number GUI"
+        JButton button = new JButton("Square"); // create a JButton object with label "Square"
+
+
+        frame.add(button); // add the button 
+        button.setBounds(200, 200, 100, 50);
+        frame.setLayout(null);
+
+        frame.setVisible(true); // make the frame visible
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the application when the user closes the frame
+        frame.setBounds(900, 540, 500, 500);
+    }
+}
